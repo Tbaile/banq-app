3dddf9d321299d98fec5eaa78a05dc4173d17302
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2025-01-06 15:28:19 +0100

    chore: updated initial view and dipendencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "it.bailettitommaso.banq"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "it.bailettitommaso.banq"
         minSdk = 27
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/it/bailettitommaso/banq/ui/BanqApp.kt
+++ app/src/main/java/it/bailettitommaso/banq/ui/BanqApp.kt
@@ -1,15 +1,26 @@
 package it.bailettitommaso.banq.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import it.bailettitommaso.banq.ui.theme.BanqTheme
 
 @Composable
-fun BanqApp () {
+@Preview(showSystemUi = true)
+fun BanqApp() {
     BanqTheme {
-        Scaffold {
-            Text(text = "Hello, Banq!")
+        Scaffold { padding ->
+            Column(modifier = Modifier.padding(padding)) {
+                Text(
+                    modifier = Modifier.padding(8.dp),
+                    text = "Hello, Banq!"
+                )
+            }
         }
     }
 }
